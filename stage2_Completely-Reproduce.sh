edcf35f7bc4b9abd9043395e1ed0a0fa11257f4c
DevanshiGupta481 <66218163+DevanshiGupta481@users.noreply.github.com>
2023-05-04 23:28:11 +0530

    Update and rename stage2.sh to stage2_Completely-Reproduce.sh

--- stage2_Completely-Reproduce.sh
+++ stage2_Completely-Reproduce.sh
@@ -23,3 +23,29 @@ bcftools mpileup -O b -o results/bcf/SRR2584866_raw.bcf \
 
 #calculate single nucleotide variant
 bcftools call --ploidy 1 -m -v -o results/vcf/SRR2584866_variants.vcf results/bcf/SRR2584866_raw.bcf 
+vcfutils.pl varFilter results/vcf/SRR2584866_variants.vcf  > results/vcf/SRR2584866_final_variants.vcf
+
+bcftools stats results/bcf/SRR2584866_variants.vcf | grep TSTV
+# TSTV, transitions/transversions:
+# TSTV	[2]id	[3]ts	[4]tv	[5]ts/tv	[6]ts (1st ALT)	[7]tv (1st ALT)	[8]ts/tv (1st ALT)
+TSTV	0	628	58	10.83	628	58	10.83
+bcftools stats results/vcf/SRR2584866_final_variants.vcf | grep TSTV
+# TSTV, transitions/transversions:
+# TSTV	[2]id	[3]ts	[4]tv	[5]ts/tv	[6]ts (1st ALT)	[7]tv (1st ALT)	[8]ts/tv (1st ALT)
+TSTV	0	621	54	11.50	621	54	11.50
+
+#explore VCF format
+less -S results/vcf/SRR2584866_final_variants.vcf
+
+#visualization
+samtools index results/bam/SRR2584866.aligned.sorted.bam
+samtools tview results/bam/SRR2584866.aligned.sorted.bam data/ref_genome/ecoli_rel606.fasta
+
+#viewing with IGV
+mkdir ~/Desktop/files_for_igv
+cd ~/Desktop/files_for_igv
+
+scp dcuser@ec2-34-203-203-131.compute-1.amazonaws.com:~/dc_workshop/results/bam/SRR2584866.aligned.sorted.bam ~/Desktop/files_for_igv
+scp dcuser@ec2-34-203-203-131.compute-1.amazonaws.com:~/dc_workshop/results/bam/SRR2584866.aligned.sorted.bam.bai ~/Desktop/files_for_igv
+scp dcuser@ec2-34-203-203-131.compute-1.amazonaws.com:~/dc_workshop/data/ref_genome/ecoli_rel606.fasta ~/Desktop/files_for_igv
+scp dcuser@ec2-34-203-203-131.compute-1.amazonaws.com:~/dc_workshop/results/vcf/SRR2584866_final_variants.vcf ~/Desktop/files_for_igv
